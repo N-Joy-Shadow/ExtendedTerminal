237cbf88f68cf77c72c664f1826bd5162c18a7ba
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2025-05-20 22:35:24 +0900

    custom slot position

--- src/main/java/com/myogoo/extendedterminal/client/screen/BasicTerminalScreen.java
+++ src/main/java/com/myogoo/extendedterminal/client/screen/BasicTerminalScreen.java
@@ -7,15 +7,21 @@ import appeng.client.gui.style.ScreenStyle;
 import appeng.client.gui.widgets.ActionButton;
 import appeng.core.AEConfig;
 import com.myogoo.extendedterminal.menu.BasicTerminalMenu;
+import com.myogoo.extendedterminal.menu.ETSlotSemantics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.Slot;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 public class BasicTerminalScreen extends MEStorageScreen<BasicTerminalMenu> {
+    private static final Logger LOGGER = Logger.getLogger(BasicTerminalScreen.class.getName());
+
     public BasicTerminalScreen(BasicTerminalMenu menu, Inventory inventory, Component title, ScreenStyle style) {
         super(menu, inventory, title, style);
-
         ActionButton clearBtn = new ActionButton(ActionItems.S_STASH, btn -> menu.clearCraftingGrid());
     }
 
@@ -26,5 +32,25 @@ public class BasicTerminalScreen extends MEStorageScreen<BasicTerminalMenu> {
         }
         super.onClose();
     }
+
+    @Override
+    public void init() {
+        super.init();
+        LOGGER.info(String.format("%d %d\n",this.leftPos, this.topPos));
+        List<Slot> craftingSlots = this.getMenu().getSlots(ETSlotSemantics.BASIC_CRAFTING_GRID);
+        int craftGridStartX = this.leftPos + 10;
+        int craftGridStartY = this.topPos + 10;
+        for(int row = 0; row < 3; row++) {
+            for(int col = 0; col < 3; col++) {
+                int index = row * 3 + col;
+                Slot slot = craftingSlots.get(index);
+                int x = craftGridStartX + col * 18;
+                int y = craftGridStartY + row * 18;
+                slot.x = x;
+                slot.y = y;
+            }
+        }
+    }
+
 
 }
